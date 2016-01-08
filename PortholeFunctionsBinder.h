25b9ef4d620d81314f69b556ce43085ee7a70a28
Alessandro Febretti <febret@gmail.com>
2016-01-08 14:58:01 -0600

    added missing header guard

--- PortholeFunctionsBinder.h
+++ PortholeFunctionsBinder.h
@@ -30,6 +30,8 @@
 * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ******************************************************************************/
+#ifndef __PORTHOLE_FUNCTIONS_BINDER__
+#define __PORTHOLE_FUNCTIONS_BINDER__
 
 #define PORTHOLE_EVENT_TOKEN_CAMERA_ID "%id%"
 #define PORTHOLE_EVENT_TOKEN_VALUE "%value%"
@@ -200,3 +202,4 @@ public:
     std::map<std::string, string> pythonFunIdMap;
     int scriptNumber;
 };
+#endif
