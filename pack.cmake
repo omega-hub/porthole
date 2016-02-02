b5d6f62faf7032b7facd6380b583ebdf99f25bf0
Alessandro Febretti <febret@gmail.com>
2016-02-01 21:31:13 -0600

    updated pack script

--- pack.cmake
+++ pack.cmake
@@ -3,10 +3,3 @@ pack_dir(res)
 if(PACK_EXAMPLES)
     pack_dir(examples)
 endif()
-
-file(INSTALL DESTINATION ${PACKAGE_DIR}/modules/porthole
-    TYPE FILE
-    FILES
-        ${MODULE_DIR}//server.cfg 
-    )
-    
